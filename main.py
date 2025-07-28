68006f80ea7f698fd4060da2f0b84b70321e2de6
SimpleCaci <edikus@outlook.es>
2025-07-28 09:09:42 -0700

    feat: MVP completed, image able to be basically preprocessed and text extracted

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import imageLoader, preprocessing, textExtractor #local files
+import debugging
 
 import numpy as np
-import cv2
+import cv2, pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 #pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image = imageLoader.loadImage("./Input/test.jpg")
+image = imageLoader.loadImage("./Input/test2.jpg")
 
-image = preprocessing.imagePreprocessing(image)
+image = preprocessing.preprocess(image)
+
+#debugging
+print("debugging")
+debugging.showImage(image, "Processed Image")
+text = pytesseract.image_to_string(image, config='--psm 6')
+print("Extracted Text:\n", text)
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,10 @@
+import cv2
+import numpy as np
 #for filters and preprocessing
 def preprocess(image):
+    resized = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)  
+    blurred = cv2.GaussianBlur(resized, (5, 5), 0)  
+    adaptive_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)  
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.morphologyEx(adaptive_thresh, cv2.MORPH_OPEN, kernel)
     return image
